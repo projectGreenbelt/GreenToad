eaf98da9476395f56ea33ee6437dc43125bdf95e
James Tobey <jctobey@eckerd.edu>
2019-01-09 14:26:51 -0600

    routes broke because we changed to exact path, and the exact path for home was rendering the home component. Fixed this and also testing the noMatch component in this push

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Router, Switch } from "react-router-dom";
 import App from "./App";
-import NoMatch from "./components/NoMatch/NoMatch";
-import Home from "./components/Home/Home";
+// import NoMatch from "./components/NoMatch/NoMatch";
+// import Home from "./components/Home/Home";
 import Callback from "./components/Callback/Callback";
 import Auth from "./components/Authorization/Authorization";
 import history from "./history";
@@ -14,33 +14,31 @@ const handleAuthentication = ({ location }) => {
     auth.handleAuthentication();
   }
 };
-
+const NoMatch = () => <h3>No match</h3>;
 export const makeMainRoutes = () => {
   return (
     <Router history={history}>
-      <div>
-        <Switch>
-          <Route
-            exact
-            path="/home"
-            render={props => <Home auth={auth} {...props} />}
-          />
-          <Route
-            exact
-            path="/"
-            render={props => <App auth={auth} {...props} />}
-          />
-          <Route
-            exact
-            path="/callback"
-            render={props => {
-              handleAuthentication(props);
-              return <Callback {...props} />;
-            }}
-          />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/home"
+          render={props => <App auth={auth} {...props} />}
+        />
+        <Route
+          exact
+          path="/"
+          render={props => <App auth={auth} {...props} />}
+        />
+        <Route
+          exact
+          path="/callback"
+          render={props => {
+            handleAuthentication(props);
+            return <Callback {...props} />;
+          }}
+        />
+        <Route component={NoMatch} />
+      </Switch>
     </Router>
   );
 };
