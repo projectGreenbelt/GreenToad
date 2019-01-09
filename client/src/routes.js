7edbe2863fd2a8609e2af523e1e807939fb22e13
James Tobey <jctobey@eckerd.edu>
2019-01-09 12:27:39 -0600

    trying to update routes so we have page not found when a route is not hit

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Route, Router } from "react-router-dom";
+import { Route, Router, Switch } from "react-router-dom";
 import App from "./App";
-import Main from "./pages/Main";
+import NoMatch from "./components/NoMatch/NoMatch";
 import Home from "./components/Home/Home";
 import Callback from "./components/Callback/Callback";
 import Auth from "./components/Authorization/Authorization";
@@ -19,15 +19,27 @@ export const makeMainRoutes = () => {
   return (
     <Router history={history}>
       <div>
-        <Route path="/home" render={props => <Home auth={auth} {...props} />} />
-        <Route path="/" render={props => <App auth={auth} {...props} />} />
-        <Route
-          path="/callback"
-          render={props => {
-            handleAuthentication(props);
-            return <Callback {...props} />;
-          }}
-        />
+        <Switch>
+          <Route
+            exact
+            path="/home"
+            render={props => <Home auth={auth} {...props} />}
+          />
+          <Route
+            exact
+            path="/"
+            render={props => <App auth={auth} {...props} />}
+          />
+          <Route
+            exact
+            path="/callback"
+            render={props => {
+              handleAuthentication(props);
+              return <Callback {...props} />;
+            }}
+          />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     </Router>
   );
