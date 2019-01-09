4c7d7b798b849c702f31846a1ab0373804ad3aec
James Tobey <jctobey@eckerd.edu>
2019-01-09 14:33:59 -0600

    reverting last change, not the routes I was looking for lol

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Router, Switch } from "react-router-dom";
 import App from "./App";
 // import NoMatch from "./components/NoMatch/NoMatch";
-// import Home from "./components/Home/Home";
+import Home from "./components/Home/Home";
 import Callback from "./components/Callback/Callback";
 import Auth from "./components/Authorization/Authorization";
 import history from "./history";
@@ -18,27 +18,17 @@ const NoMatch = () => <h3>No match</h3>;
 export const makeMainRoutes = () => {
   return (
     <Router history={history}>
-      <Switch>
+      <div>
+        <Route path="/home" render={props => <Home auth={auth} {...props} />} />
+        <Route path="/" render={props => <App auth={auth} {...props} />} />
         <Route
-          exact
-          path="/home"
-          render={props => <App auth={auth} {...props} />}
-        />
-        <Route
-          exact
-          path="/"
-          render={props => <App auth={auth} {...props} />}
-        />
-        <Route
-          exact
           path="/callback"
           render={props => {
             handleAuthentication(props);
             return <Callback {...props} />;
           }}
         />
-        <Route component={NoMatch} />
-      </Switch>
+      </div>
     </Router>
   );
 };
