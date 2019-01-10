3669b1318fd1239a787f3830db38e9b0459fa759
James Tobey <jctobey@eckerd.edu>
2019-01-09 21:42:12 -0600

    trying to import theme into routes file

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,7 +6,24 @@ import Home from "./components/Home/Home";
 import Callback from "./components/Callback/Callback";
 import Auth from "./components/Authorization/Authorization";
 import history from "./history";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#6fbf73",
+      main: "#66BB6A",
+      dark: "#357a38",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000"
+    }
+  }
+});
 const auth = new Auth();
 
 const handleAuthentication = ({ location }) => {
@@ -17,26 +34,28 @@ const handleAuthentication = ({ location }) => {
 
 export const makeMainRoutes = () => {
   return (
-    <Router history={history}>
-      <div>
-        <Route
-          exact
-          path="/home"
-          render={props => <App auth={auth} {...props} />}
-        />
-        <Route
-          exact
-          path="/"
-          render={props => <App auth={auth} {...props} />}
-        />
-        <Route
-          path="/callback"
-          render={props => {
-            handleAuthentication(props);
-            return <Callback {...props} />;
-          }}
-        />
-      </div>
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router history={history}>
+        <div>
+          <Route
+            exact
+            path="/home"
+            render={props => <App auth={auth} {...props} />}
+          />
+          <Route
+            exact
+            path="/"
+            render={props => <App auth={auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => {
+              handleAuthentication(props);
+              return <Callback {...props} />;
+            }}
+          />
+        </div>
+      </Router>
+    </MuiThemeProvider>
   );
 };
