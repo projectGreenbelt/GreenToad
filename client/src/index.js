582480fe1e789374d25c6073629cb8cc0137034b
Marcus Aurelius <markmayfield16@gmail.com>
2019-01-11 19:35:16 -0600

    testing responsive

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
 import { makeMainRoutes } from "./routes";
 import * as serviceWorker from "./serviceWorker";
 const routes = makeMainRoutes();
