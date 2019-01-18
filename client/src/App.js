3ff3ae20f69f5ac13c4861c9f153e35d0c4ede7a
James Tobey <jctobey@eckerd.edu>
2019-01-18 16:29:50 -0600

    redirect component complete, also added fun mario gif because the page transition is really quick without some sort of time out.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+// import Nav from "./components/Nav/Nav";
 // import { Navbar, Button } from "react-bootstrap";
 // import Auth from "./components/Authorization/Authorization";
 import Main from "./pages/Main";
 import Profile from "./components/Profile/Profile";
-import Social from "./pages/Social";
 import "./App.css";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -13,54 +13,8 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-
-//Material UI Icons for Menu
-import AccountBalance from "@material-ui/icons/AccountBalance";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Fingerprint from "@material-ui/icons/Fingerprint";
-import Person from "@material-ui/icons/Person";
-
-
-const styles = theme => ({
-  menuItem: {
-    '&:hover': {
-      backgroundColor: theme.palette.primary.light,
-      '& $primary, & $icon': {
-        color: theme.palette.common.white,
-      },
-    },
-  },
-  primary: {},
-  icon: {},
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -10,
-    marginRight: 0,
-  },
-});
 
 class App extends Component {
-  state = {
-    anchorEl: null,
-  };
-
-  handleClick = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
   goTo(route) {
     this.props.history.replace(`/${route}`);
   }
@@ -99,7 +53,6 @@ class App extends Component {
     const { isAuthenticated } = this.props.auth;
     console.log(isAuthenticated());
     const { classes } = this.props;
-    const { anchorEl } = this.state;
 
     return (
       <div>
@@ -138,78 +91,38 @@ class App extends Component {
           </Navbar.Header>
         </Navbar> */}
         {withStyles}
-
-        <div className={classes.root}>
+        <div className={styles.root}>
           <AppBar position="static">
             <Toolbar className="theme">
-              <Typography variant="h6" color="inherit" className={classes.grow}>
+              <Typography variant="h6" color="inherit" className={styles.grow}>
                 Project GreenBelt
               </Typography>
-              <Button onClick={this.login.bind(this)} color="inherit">
-                Login
-              </Button>
+              {isAuthenticated() && (
+                <Button onClick={this.logout.bind(this)} color="inherit">
+                  Logout
+                </Button>
+              )}
+              {!isAuthenticated() && (
+                <Button onClick={this.login.bind(this)} color="inherit">
+                  Login
+                </Button>
+              )}
+
               <IconButton
                 className={styles.menuButton}
                 color="inherit"
                 aria-label="Menu"
-                aria-owns={anchorEl ? 'simple-menu' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleClick}
               >
                 <MenuIcon />
               </IconButton>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={this.handleClose}
-              >
-                <MenuItem 
-                  onClick={this.handleClose} 
-                  className={classes.menuItem}>
-                  <ListItemIcon className={classes.icon}>
-                    <Person />
-                  </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Profile" />
-                </MenuItem>
-                <MenuItem 
-                  onClick={this.handleClose} 
-                  className={classes.menuItem}>
-                  <ListItemIcon className={classes.icon}>
-                    <AccountBalance />
-                  </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Home" />
-                </MenuItem>
-                <MenuItem 
-                  onClick={this.handleClose}
-                  className={classes.menuItem}>
-                  <ListItemIcon className={classes.icon}>
-                    <LocationOn />
-                  </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Location" />
-                </MenuItem>
-                <MenuItem 
-                  onClick={this.handleClose}
-                  className={classes.menuItem}>
-                  <ListItemIcon className={classes.icon}>
-                    <Fingerprint />
-                  </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Logout" />
-                </MenuItem>
-              </Menu>
             </Toolbar>
           </AppBar>
           {/* <Nav /> */}
         </div>
         {isAuthenticated() && <Main />}
-        <Social />
       </div>
-    )
+    );
   }
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(App);
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
+// import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { makeMainRoutes } from "./routes";
 import * as serviceWorker from "./serviceWorker";
-
-
 const routes = makeMainRoutes();
 
 ReactDOM.render(routes, document.getElementById("root"));
