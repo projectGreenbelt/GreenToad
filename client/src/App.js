155f02ce6ea32ddcc649cce875cf97c3529ba4f6
James Tobey <jctobey@eckerd.edu>
2019-01-18 21:06:20 -0600

    did some reset stuff, see if that pushes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 // import { Navbar, Button } from "react-bootstrap";
 // import Auth from "./components/Authorization/Authorization";
 import Main from "./pages/Main";
@@ -13,10 +13,10 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 //Material UI Icons for Menu
 import AccountBalance from "@material-ui/icons/AccountBalance";
@@ -24,33 +24,32 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import Fingerprint from "@material-ui/icons/Fingerprint";
 import Person from "@material-ui/icons/Person";
 
-
 const styles = theme => ({
   menuItem: {
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.primary.light,
-      '& $primary, & $icon': {
-        color: theme.palette.common.white,
-      },
-    },
+      "& $primary, & $icon": {
+        color: theme.palette.common.white
+      }
+    }
   },
   primary: {},
   icon: {},
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   grow: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
     marginLeft: -10,
-    marginRight: 0,
-  },
+    marginRight: 0
+  }
 });
 
 class App extends Component {
   state = {
-    anchorEl: null,
+    anchorEl: null
   };
 
   handleClick = event => {
@@ -152,7 +151,7 @@ class App extends Component {
                 className={styles.menuButton}
                 color="inherit"
                 aria-label="Menu"
-                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-owns={anchorEl ? "simple-menu" : undefined}
                 aria-haspopup="true"
                 onClick={this.handleClick}
               >
@@ -164,52 +163,71 @@ class App extends Component {
                 open={Boolean(anchorEl)}
                 onClose={this.handleClose}
               >
-                <MenuItem 
-                  onClick={this.handleClose} 
-                  className={classes.menuItem}>
+                <MenuItem
+                  onClick={this.handleClose}
+                  className={classes.menuItem}
+                >
                   <ListItemIcon className={classes.icon}>
                     <Person />
                   </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Profile" />
+                  <ListItemText
+                    classes={{ primary: classes.primary }}
+                    inset
+                    primary="Profile"
+                  />
                 </MenuItem>
-                <MenuItem 
-                  onClick={this.handleClose} 
-                  className={classes.menuItem}>
+                <MenuItem
+                  onClick={this.handleClose}
+                  className={classes.menuItem}
+                >
                   <ListItemIcon className={classes.icon}>
                     <AccountBalance />
                   </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Home" />
+                  <ListItemText
+                    classes={{ primary: classes.primary }}
+                    inset
+                    primary="Home"
+                  />
                 </MenuItem>
-                <MenuItem 
+                <MenuItem
                   onClick={this.handleClose}
-                  className={classes.menuItem}>
+                  className={classes.menuItem}
+                >
                   <ListItemIcon className={classes.icon}>
                     <LocationOn />
                   </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Location" />
+                  <ListItemText
+                    classes={{ primary: classes.primary }}
+                    inset
+                    primary="Location"
+                  />
                 </MenuItem>
-                <MenuItem 
+                <MenuItem
                   onClick={this.handleClose}
-                  className={classes.menuItem}>
+                  className={classes.menuItem}
+                >
                   <ListItemIcon className={classes.icon}>
                     <Fingerprint />
                   </ListItemIcon>
-                  <ListItemText classes={{ primary: classes.primary }} inset primary="Logout" />
+                  <ListItemText
+                    classes={{ primary: classes.primary }}
+                    inset
+                    primary="Logout"
+                  />
                 </MenuItem>
               </Menu>
             </Toolbar>
           </AppBar>
-          {/* <Nav /> */}
         </div>
         {isAuthenticated() && <Main />}
         <Social />
       </div>
-    )
+    );
   }
 }
 
 App.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(App);
