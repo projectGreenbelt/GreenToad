a54f08d7e5fbb5ebe76b76872ff43f8d6b4e466a
James <jctobey@eckerd.edu>
2019-01-28 21:36:26 -0600

    changed to callback auth0 variable

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "projectgreenbelt.auth0.com",
   clientId: "6CbMurlyapmjLnElZXlWfg76yliVOx-r",
-  callbackUrl: "https://mark-greenbelt-staging.herokuapp.com/callback"
+  callbackUrl: "https://greenbelt-staging-app.herokuapp.com/callback"
 };
