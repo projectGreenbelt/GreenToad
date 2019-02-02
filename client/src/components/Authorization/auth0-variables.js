eaf2106fb56a7d4b3748ed113e91a5ff2861d6bd
Marcus Aurelius <markmayfield16@gmail.com>
2019-02-02 16:11:25 -0600

    testing out directions

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,7 +1,8 @@
 export const AUTH_CONFIG = {
   domain: "projectgreenbelt.auth0.com",
   clientId: "6CbMurlyapmjLnElZXlWfg76yliVOx-r",
-  callbackUrl: "http://localhost:3000/callback"
+  callbackUrl: "https://mark-greenbelt-staging.herokuapp.com/callback"
 };
 
 /* https://mark-greenbelt-staging.herokuapp.com/callback */
+/* http://localhost:3000/callback */
