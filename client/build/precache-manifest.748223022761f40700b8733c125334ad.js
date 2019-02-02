7a2c8f30a12eb5fbbf5b8a605e90c7126f9da321
Marcus Aurelius <markmayfield16@gmail.com>
2019-02-02 15:36:16 -0600

    testing out directions on home page

---
+++ client/build/precache-manifest.748223022761f40700b8733c125334ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5933790c3250a5b565ff53d1c7709fc9",
+    "url": "/static/media/Test.5933790c.jpg"
+  },
+  {
+    "revision": "17d1551724c73cc2f9c02d93089560e2",
+    "url": "/static/media/Social2.17d15517.jpg"
+  },
+  {
+    "revision": "b09ab93c9b49694b78380ace251d1426",
+    "url": "/static/media/Profile.b09ab93c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de25bd308e8c14cb6aaf",
+    "url": "/static/js/main.de25bd30.chunk.js"
+  },
+  {
+    "revision": "e58f9716b4235445fce2",
+    "url": "/static/js/1.e58f9716.chunk.js"
+  },
+  {
+    "revision": "de25bd308e8c14cb6aaf",
+    "url": "/static/css/main.55f2e023.chunk.css"
+  },
+  {
+    "revision": "8d7636fc8e236de318158537ed09a708",
+    "url": "/index.html"
+  }
+];
