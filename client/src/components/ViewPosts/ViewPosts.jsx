f7b1e3464c79219d6021fd96eea8ac816ef36f62
Marcus Aurelius <markmayfield16@gmail.com>
2019-02-02 15:40:12 -0600

    testing directions

---
+++ client/src/components/ViewPosts/ViewPosts.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { List, ListItem } from "../PostsList/PostsList";
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 2,
+    flexGrow: 1,
+    overflow: "hidden"
+  },
+  paper: {
+    margin: `${theme.spacing.unit}px auto`,
+    padding: theme.spacing.unit * 2,
+    maxWidth: 300,
+    minwidth: 275
+  },
+  list: {
+    margin: `${theme.spacing.unit}px auto`,
+    padding: theme.spacing.unit * 2,
+    maxWidth: 500,
+    minwidth: 275,
+    height: 600
+  }
+});
+
+function PaperSheet(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.paper} elevation={20}>
+        <Grid container wrap="nowrap" spacing={16}>
+          <Grid item xs>
+            <Typography variant="h5" component="h3">
+              Current Location Updates:
+            </Typography>
+            <hr />
+            <Paper className={classes.list} elevation={20}>
+              <List>
+                {props.otherPosts.map(post => {
+                  return <ListItem key={post._id}>{post.post}</ListItem>;
+                })}
+              </List>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+PaperSheet.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(PaperSheet);
