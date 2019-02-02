19f123e68e77174c2ab1e053e40da57766dbe351
Marcus Aurelius <markmayfield16@gmail.com>
2019-02-02 16:33:31 -0600

    merged with master

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,13 +1,8 @@
 export const AUTH_CONFIG = {
   domain: "projectgreenbelt.auth0.com",
   clientId: "6CbMurlyapmjLnElZXlWfg76yliVOx-r",
-<<<<<<< HEAD
   callbackUrl: "https://mark-greenbelt-staging.herokuapp.com/callback"
 };
 
 /* https://mark-greenbelt-staging.herokuapp.com/callback */
 /* http://localhost:3000/callback */
-=======
-  callbackUrl: "https://greentoad.herokuapp.com/callback"
-};
->>>>>>> d460a86301e63f21424bf1b6feda15960747f579
