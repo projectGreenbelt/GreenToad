81a90b45e1f198ae99f748d9eb81228a7c50b0af
Marcus Aurelius <markmayfield16@gmail.com>
2019-04-13 12:42:16 -0500

    staging for deployment

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
   domain: "projectgreenbelt.auth0.com",
   clientId: "6CbMurlyapmjLnElZXlWfg76yliVOx-r",
-  callbackUrl: "http://localhost:3000/callback"
+  callbackUrl: "https:/greentoad55.herokuapp.com/callback"
 };
 
 
