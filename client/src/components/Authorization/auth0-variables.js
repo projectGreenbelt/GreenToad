68b04db42dcb451ab405af240abb4d41937cbf61
Marcus Aurelius <markmayfield16@gmail.com>
2019-04-13 12:50:36 -0500

    staging for deployment, added href back to landing button for css on mobile

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,10 +1,10 @@
 export const AUTH_CONFIG = {
   domain: "projectgreenbelt.auth0.com",
   clientId: "6CbMurlyapmjLnElZXlWfg76yliVOx-r",
-  callbackUrl: "https:/greentoad55.herokuapp.com/callback"
+  callbackUrl: "https://greentoad55.herokuapp.com/callback"
 };
 
 
 //https://mark-greenbelt-staging.herokuapp.com/callback
 //http://localhost:3000/callback
-//https:/greentoad55.herokuapp.com/callback
+//https://greentoad55.herokuapp.com/callback
