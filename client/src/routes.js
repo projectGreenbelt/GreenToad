4d719d4c33ab985241095f7e4f2cb79c2ed8c102
Marcus Aurelius <markmayfield16@gmail.com>
2019-06-01 12:10:27 -0500

    Staging Once Again

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile/Profile";
 import Callback from "./components/Callback/Callback";
