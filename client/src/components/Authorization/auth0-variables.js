86dc998ac4c49859137dd1ed75287139e1a39c81
James Tobey <41517616+jctobey@users.noreply.github.com>
2019-12-10 22:22:31 -0600

    Update auth0-variables.js

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "projectgreenbelt.auth0.com",
   clientId: "6CbMurlyapmjLnElZXlWfg76yliVOx-r",
-  callbackUrl: "https://greentoad.herokuapp.com/callback"
+  callbackUrl: "https://greentoadapp.herokuapp.com/callback"
 };
